6dff0dba0287500f5ffef83917ae2596d4d8c4e7
Nobuo Kato <katonobu4649@gmail.com>
2023-01-27 12:58:25 +0000

    Update scripts for WSL environment

--- tools/python/wsl/watch_and_copy_to_win.py
+++ tools/python/wsl/watch_and_copy_to_win.py
@@ -1,7 +1,12 @@
 import os
+import sys
 import time
 import datetime
 import shutil
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from lib.FileDriveStt import DriveStt,FileStt
+from lib.EnvUtil import EnvUtil
 
 ###########################
 repo_base_in_win = '/mnt/c/msys64/home/katon/pico-dev_docker'
@@ -21,63 +26,36 @@ repo_base_in_linux = os.sep.join([
 target_path = repo_base_in_win + '/tools/python/win/uf2'
 build_base_path = repo_base_in_linux + '/pico/build_rzppw'
 
-class FileStt():
-    _build_base_path = None
-    _target_path = None
-
-    @classmethod
-    def set_build_base_path(cls, build_base_path):
-        cls._build_base_path = build_base_path
-
-    @classmethod
-    def set_target_path(cls, target_path):
-        cls._target_path = target_path
-
-    def __init__(self, src_file):
-        self.src_file_path = os.sep.join([FileStt._build_base_path, src_file])
-        if os.path.isfile(self.src_file_path):
-            stat = os.stat(self.src_file_path)
-            self.current_ts = stat.st_mtime
-            self.prev_ts = stat.st_mtime
-        else:
-            self.current_ts = 0
-            self.prev_ts = 0
-
-    def update(self):
-        updated = False
-        if os.path.isfile(self.src_file_path):
-            stat = os.stat(self.src_file_path)
-            self.prev_ts = self.current_ts
-            self.current_ts = stat.st_mtime
-            if self.prev_ts < self.current_ts:
-                updated = True
-        return updated
-
-    def print(self):
-        print("{}".format(self.src_file_path.split(os.sep)[-1]))
-        if self.prev_ts is not None:
-            print("  prev_ts   :{}".format(datetime.datetime.fromtimestamp(self.prev_ts).strftime('%Y/%m/%d %H:%M:%S')))
-        else:
-            print("  prev_ts   :None")
-        if self.current_ts is not None:
-            print("  current_ts:{}".format(datetime.datetime.fromtimestamp(self.current_ts).strftime('%Y/%m/%d %H:%M:%S')))
-        else:
-            print("  current_ts:None")
-
 if __name__ == "__main__" :
-    if not os.path.isdir(target_path):
-        os.makedirs(target_path)
+    env = EnvUtil()
 
-    FileStt.set_build_base_path(build_base_path)
-    FileStt.set_target_path(target_path)
+    if env.isRunningOnWsl:
+        if not os.path.isdir(target_path):
+            os.makedirs(target_path)
 
-    fileStts = [FileStt(f) for f in uf2_paths]
+        FileStt.set_base_path(build_base_path)
 
-    print("Start : {}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y/%m/%d %H:%M:%S')))
-    while True:
-        for fileStt in fileStts:
-            if fileStt.update():
-                fileStt.print()
-                shutil.copyfile(fileStt.src_file_path, target_path + os.sep + fileStt.src_file_path.split(os.sep)[-1])
-                print("------------")
-        time.sleep(1)
+        fileStts = [FileStt(f) for f in uf2_paths]
+
+        print("Start : {}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y/%m/%d %H:%M:%S')))
+        while True:
+            for fileStt in fileStts:
+                if fileStt.update():
+                    fileStt.print()
+                    print("------------")
+
+            while True:
+                try:
+                    src = FileStt.updates.pop()
+                    shutil.copyfile(src, target_path + os.sep + src.split(os.sep)[-1])
+                except IndexError:
+                    break
+
+            time.sleep(1)
+    else:
+        print("This script assumes run on WSL.")
+        if env.isRunningOnDocker:
+            print("  You runs this script on Docker-container")
+        elif env.isWindowsOs:
+            print("  You runs this script on Windows")
+
